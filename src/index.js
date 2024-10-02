c8acd80bf78900728a2bb48e100ae797bb3b48ad
RayWZW <harrysmith690089@gmail.com>
2024-10-01 20:36:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ import trollIframe from "./Mods/BB";
 import meetingRecorder from "./Mods/meetingrecorder";
 import clone from "./Mods/clone";
 import "./config";
+import nameChange from "./Mods/nameChange";
 
 const bannerUrl = 'https://us04images.zoom.us/p/v2/8549d108896fd231d80a827f65472f1d43c26f55457be4d9cee1b894f1dfee1f/135b3d03-55be-4bb7-afe2-5789e89b4b57?type=large&_=5352'; 
-
 let menu = Menu("THUGWARE", bannerUrl); 
+
+
 
 Menu.addSeparator(menu);
 Menu.addSeparator(menu);
@@ -28,11 +30,21 @@ Menu.addButton(menu, 'BYPASS KICK', false, () => {
     sessionStorage.clear();
     core.frame.location.reload();
 });
+Menu.addSeparator(menu); 
+Menu.addSeparator(menu); 
+Menu.addButton(menu, 'Impersonate Guests', true, nameSpammer, nameSpammer.stop);
+Menu.addButton(menu, 'Speed Rename', true, nameChange, nameChange.stop);
+Menu.addSeparator(menu); 
+Menu.addSeparator(menu); 
+
 Menu.addSeparator(menu);
 Menu.addButton(menu, 'Background Bots', false, () => {
     trollIframe(false);
-});
-Menu.addButton(menu, 'Impersonate Guests', true, nameSpammer, nameSpammer.stop);
+}, () => {
+    console.log('Button disabled!');
+}, false, 'PAIR WITH SPEED RENAME FOR BEST RESULTS'); // Tooltip text
+
+
 Menu.addButton(menu, 'Raise Hand Spammer', true, handSpammer, handSpammer.stop);
 Menu.addButton(menu, 'Bot Meeting', false, botMeeting);
 Menu.addButton(menu, 'Reaction Spammer', true, reactionSpammer, reactionSpammer.stop);
@@ -42,7 +54,9 @@ Menu.addButton(menu, 'Duplicate Meeting', true, () => {
     clone(amount); 
 });
 
+
 Menu.addSeparator(menu); 
 Menu.addButton(menu, 'DISCORD', false, () => {
     window.open('https://discord.gg/bosinn', '_blank'); 
 });
+
