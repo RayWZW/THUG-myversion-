381688fa9e32f844a022fde1cf6f1e9d42eee91f
RayWZW <harrysmith690089@gmail.com>
2024-10-01 20:37:23 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: 'production', 
+  entry: './src/index.js', 
   output: {
-    path: path.resolve(__dirname, 'Build'),
-    filename: 'Thug.min.js',
+    path: path.resolve(__dirname, 'Build'), 
+    filename: 'Thug.min.js', 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: {
+          loader: 'babel-loader', 
+          options: {
+            presets: ['@babel/preset-env'], 
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'], 
   },
 };
